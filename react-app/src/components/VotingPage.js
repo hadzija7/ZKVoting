3f57b199d7e983f80bb035b7927a691f71881d04
hadzija7 <hadzijasale@gmail.com>
2022-01-30 15:58:24 +0100

    wip

--- react-app/src/components/VotingPage.js
+++ react-app/src/components/VotingPage.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getVotingProcess } from '../web3/contracts';
+import Collapse from 'react-bootstrap/Collapse';
 
 import styles from './VotingPage.module.css';
 
@@ -8,12 +9,18 @@ import { getVotingProcessContract } from '../web3/contracts';
 import { initLocalStorage, generateIdentityCommitment } from '../web3/semaphore'
 
 const VotingPage = () => {
+
+    const circuitUrl = "https://semaphoreui.blob.core.windows.net/snarks/circuit.json"
+    const provingKeyUrl = "https://semaphoreui.blob.core.windows.net/snarks/proving_key.bin"
+
     const { id } = useParams()
 
     const [votingProcess, setVotingProcess] = useState(null);
     const [identityCommitment, setIdentityCommitment] = useState(null);
     const [hasRegistered, setHasRegistered] = useState(false);
     const [hasCheckedRegistration, setHasCheckedRegistration] = useState(false);
+    const [open, setOpen] = useState(true);
+    const [vote, setVote] = useState('');
 
     useEffect(() => {
         getVotingProcess(id).then((result) => {
@@ -26,10 +33,16 @@ const VotingPage = () => {
         console.log("Identity commitment: ", identityCommitment);
     }, []);
 
+    const fetchWithoutCache = (
+        url,
+    ) => {
+        return fetch(url, { cache: 'no-store' })
+    }
+
     const renderRegisterButton = () => {
         let button;
         if (hasRegistered){
-            button = <button onClick={handleRegisterClick} className="greenButton">Registered</button>
+            button = <button disabled={true} className="greenButton">Registered</button>
         }else {
             button = <button onClick={handleRegisterClick} className="baseButton">Register identity</button>
         }
@@ -44,14 +57,37 @@ const VotingPage = () => {
         console.log(receipt)
 
         if (receipt.status === 1) {
-            setHasRegistered(true)
-            setHasCheckedRegistration(true)
-            
+            setHasRegistered(true);
+            setHasCheckedRegistration(true);
+            setOpen(true);
         }else{
 
         }
     }
 
+    const handleVoteClick = async () => {
+        console.log('Downloading circuit from', circuitUrl)
+
+        const cirDef = await (await fetchWithoutCache(circuitUrl)).json() 
+        console.log("Downloaded circuit: ", cirDef);
+        // const circuit = genCircuit(cirDef)
+        
+
+        console.log('Downloading proving key from', provingKeyUrl)
+        // const provingKey = new Uint8Array(
+        //     await (await fetch(provingKeyUrl)).arrayBuffer()
+        // )
+        const toBuffer = function(ab) {
+            const buf = Buffer.alloc(ab.byteLength);
+            const view = new Uint8Array(ab);
+            for (let i = 0; i < buf.length; ++i) {
+                buf[i] = view[i];
+            }
+            return buf;
+        }
+        const provingKey = toBuffer((await (await fetch(provingKeyUrl)).arrayBuffer()))
+        console.log("Proving key: ", provingKey);
+    }
 
     return (  
         <div className={styles.container}>
@@ -63,6 +99,27 @@ const VotingPage = () => {
                 <div>
                     {renderRegisterButton()}
                 </div>
+                { true && <Collapse in={open}>
+                    <div id="example-collapse-text" className={styles.collapse}>
+                        <div>
+                            Successfuly registered, let's vote now.
+                        </div>
+                        <div className={styles.proposalsContainer}>
+                            {console.log(votingProcess.proposals)}
+                        {votingProcess.proposals.map( (proposal) => (
+                            <div key={proposal}>
+                                <input value={proposal} type="radio" name="vote"/>
+                                {proposal}
+                            </div>
+                        ))}
+                            
+                        </div>
+                        <div style={{marginTop: "2em"}}>
+                            <button onClick={handleVoteClick} className="baseButton">Vote</button>
+                        </div>
+                    </div>
+                    
+                </Collapse>}
             </div>}
         </div>
     );
